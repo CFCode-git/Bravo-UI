0ffd4e7bf99e73d007b6babe2923c1519dcca266
CFCode-git <435184766@qq.com>
2020-04-24 22:08:13 +0800

    初步完成row col 组件

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
-
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('b-button', Button)
 Vue.component('b-icon', Icon)
 Vue.component('b-button-group', ButtonGroup)
 Vue.component('b-input', Input)
+Vue.component('b-row', Row)
+Vue.component('b-col', Col)
 
 new Vue({
   el: '#app',
@@ -16,7 +19,7 @@ new Vue({
     loading1: false,
     loading2: false,
     loading3: false,
-    message:'hi'
+    message: 'hi'
   },
   methods: {
     inputChange() {
