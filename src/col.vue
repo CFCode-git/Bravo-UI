615bb5222552571cca6c4c010927f81d765a1309
CFCode-git <435184766@qq.com>
2020-05-04 22:52:29 +0800

    默认手机样式

--- src/col.vue
+++ src/col.vue
@@ -20,7 +20,6 @@
     props: {
       span: {type: [Number, String]},
       offset: {type: [Number, String]},
-      phone: {type: Object, validator},
       ipad: {type: Object, validator},
       narrowPc: {type: Object, validator},
       pc: {type: Object, validator},
@@ -39,11 +38,10 @@
         }
       },
       colClass() {
-        let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
+        let {span, offset, ipad, narrowPc, pc, widePc} = this
         return [
           span && `col-${span}`,
           offset && `offset-${offset}`,
-          ...(phone ? [`col-phone-${phone.span}`]:[]),
           ...(ipad ? [`col-ipad-${ipad.span}`]:[]),
           ...(narrowPc ? [`col-narrowPc-${narrowPc.span}`]:[]),
           ...(pc ? [`col-pc-${pc.span}`]:[]),
@@ -68,21 +66,6 @@
         margin-left: ($n/24) * 100%;
       }
     }
-    @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n/24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n/24) * 100%;
-        }
-      }
-    }
-
     @media (min-width: 577px) and (max-width: 768px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
