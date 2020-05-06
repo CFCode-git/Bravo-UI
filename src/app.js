b1444e87b544ff3e2a28658f09b2b788751b8bca
CFCode-git <435184766@qq.com>
2020-05-06 21:14:52 +0800

    为 sider 添加 slide 动画

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
 import Row from './row.vue'
 import Col from './col.vue'
+import Layout from './layout.vue'
+import Header from './header.vue'
+import Sider from './sider.vue'
+import Content from './content.vue'
+import Footer from './footer.vue'
 
 Vue.component('b-button', Button)
 Vue.component('b-icon', Icon)
@@ -12,6 +17,12 @@ Vue.component('b-button-group', ButtonGroup)
 Vue.component('b-input', Input)
 Vue.component('b-row', Row)
 Vue.component('b-col', Col)
+Vue.component('b-layout', Layout)
+Vue.component('b-header', Header)
+Vue.component('b-content', Content)
+Vue.component('b-footer', Footer)
+Vue.component('b-sider', Sider)
+
 
 new Vue({
   el: '#app',
